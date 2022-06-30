c390ae73056c9a5692bbc171c4462caa7d139fdd
Dominique Klevinghaus <79855242+Klekum@users.noreply.github.com>
2022-06-30 15:14:41 +0200

    pass file object directly

--- lib/ba_upload.rb
+++ lib/ba_upload.rb
@@ -2,8 +2,8 @@ require "ba_upload/version"
 require "openssl"
 
 module BaUpload
-  def self.export_certificate(file_path:, passphrase:)
-    cert = OpenSSL::PKCS12.new(File.read(file_path), passphrase)
+  def self.export_certificate(file:, passphrase:)
+    cert = OpenSSL::PKCS12.new(file, passphrase)
     {
       key: Tempfile.new(['key','.pem']).tap{|f| f.write(cert.key.to_s); f.flush},
       cert: Tempfile.new(['cert','.pem']).tap{|f| f.write(cert.certificate.to_s); f.flush},
@@ -11,8 +11,8 @@ module BaUpload
     }
   end
 
-  def self.open_connection(file_path:, passphrase:)
-    cert = BaUpload.export_certificate(file_path: file_path, passphrase: passphrase)
+  def self.open_connection(file:, passphrase:)
+    cert = BaUpload.export_certificate(file: file, passphrase: passphrase)
     BaUpload::Connection.new(cert[:key], cert[:cert], cert[:ca_cert])
   end
 
